b194fb7658aa17a20f6d817f5dfa427cccb8274e
Tihomir Krastev <tihomir.krastev@mail.bg>
2015-07-18 12:19:41 +0300

    ClassInfo exercise

---
+++ Iterators/src/ClassInfo.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+
+public class ClassInfo {
+	
+	private String classNumber;
+	private String roomNumber;
+	private String classManager;
+	private ArrayList<Students> students;
+	private ArrayList<Teacher> teachers;
+	
+	public ClassInfo(String classNumber, String roomNumber, String classManager) {
+		super();
+		this.students = new ArrayList<>();
+		this.teachers = new ArrayList<>();
+		this.classNumber = classNumber;
+		this.roomNumber = roomNumber;
+		this.classManager = classManager;
+	}
+	
+	public void addStudent(Students stud) {
+		this.students.add(stud);
+	}
+	
+	public void addTeacher(Teacher teach) {
+		this.teachers.add(teach);
+	}
+	
+	public double classGPA() {
+		double gpa = 0;
+		int count = 0;
+		
+		for (int i = 0; i < students.size(); i++) {
+			gpa = gpa + students.get(i).getGpa();
+			count++;
+		}
+		
+		return (gpa/count);
+	}
+	
+	public int allTeachersExp() {
+		int years = 0;
+		for (int i = 0; i < teachers.size(); i++) {
+			years = years + teachers.get(i).getExp();
+		}
+		return years;
+	}
+	
+	public ArrayList<Human> gotBirthday(String date) {
+		ArrayList<Human> allBirthdays = new ArrayList<>();
+		
+		for (int i = 0; i < teachers.size(); i++) {
+			if (date.equals(teachers.get(i).getBornDate())) {
+				allBirthdays.add(teachers.get(i));
+			}
+		}
+		
+		for (int i = 0; i < students.size(); i++) {
+			if (date.equals(students.get(i).getBornDate())) {
+				allBirthdays.add(students.get(i));
+			}
+		}
+		
+		return allBirthdays;
+		
+	}
+	
+	public String getClassNumber() {
+		return classNumber;
+	}
+	public void setClassNumber(String classNumber) {
+		this.classNumber = classNumber;
+	}
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public String getClassManager() {
+		return classManager;
+	}
+	public void setClassManager(String classManager) {
+		this.classManager = classManager;
+	}
+	
+
+}
---
+++ Iterators/src/Test.java
@@ -0,0 +1,32 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassInfo myClass = new ClassInfo("12A", "205", "Georgi Petkov");
+		Students student1 = new Students("Ivo Ivov", "26.01.1990", 3.37);
+		Students student2 = new Students("Ivan Ivov", "28.03.1990", 4.40);
+		Students student3 = new Students("Ivailo Ivov", "11.02.1990", 5.83);
+		
+		Teacher teach1 = new Teacher("Georgi Borisov", "28.01.1966", 12);
+		Teacher teach2 = new Teacher("Bobi Bobov", "23.02.1971", 10);
+		Teacher teach3 = new Teacher("Miro Mironov", "31.01.1976", 9);
+		
+		myClass.addStudent(student1);
+		myClass.addStudent(student2);
+		myClass.addStudent(student3);
+		myClass.addTeacher(teach1);
+		myClass.addTeacher(teach2);
+		myClass.addTeacher(teach3);
+		
+		System.out.println(myClass.classGPA());
+		System.out.println(myClass.allTeachersExp());
+		
+		int count = myClass.gotBirthday("28.03.1990").size();
+		for (int j = 0; j < count; j++) {
+			System.out.println(myClass.gotBirthday("28.03.1990").get(j).getName());
+		}
+		
+		
+	}
+
+}
