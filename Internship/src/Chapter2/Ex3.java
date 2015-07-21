0deac24cf3a404fde92395ad9b67f0fb9db54311
Tihomir Krastev <tihomir.krastev@mail.bg>
2015-07-21 14:39:19 +0300

    remove

--- Internship/src/Chapter2/Ex3.java
+++
@@ -1,9 +0,0 @@
-package Chapter2;
-
-public class Ex3 {
-
-	public static void main(String[] args) {
-		int a = 256;
-	}
-
-}
--- Internship/src/Chapter2/Ex4.java
+++
@@ -1,9 +0,0 @@
-package Chapter2;
-
-public class Ex4 {
-
-	public static void main(String[] args) {
-		boolean isMale = true;
-	}
-
-}
